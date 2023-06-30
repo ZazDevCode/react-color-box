88aea11189ef20d091de5599758134a523543de8
Sascha Wernars <s@scha.nl>
2023-06-30 11:48:46 +0200

    ColorBox app werkt

---
+++ src/Square.js
@@ -0,0 +1,20 @@
+function Square({ colorValue, hexValue, isDarkText }) {
+  return (
+    <section
+      className="square"
+      style={{
+        backgroundColor: colorValue,
+        color: isDarkText ? "#000" : "#FFF",
+      }}
+    >
+      <p>{colorValue ? colorValue : "Empty Value"}</p>
+      <p>{hexValue ? hexValue : null}</p>
+    </section>
+  );
+}
+
+Square.defaultProps = {
+  colorValue: "Empty Volor Value",
+};
+
+export default Square;
